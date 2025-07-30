867780cc94b10262bb430e28a87b3672d2a7777b
Ahmed Fathallah Khalil <Ahmed.fathalla@thirdwayv.com>
2025-07-30 13:40:21 +0300

    main: Add Some comments to sse example.

--- ServerSendEventsDesignPattern/index.js
+++ ServerSendEventsDesignPattern/index.js
@@ -23,8 +23,14 @@ app.get('/stream', (req, res) => {
 let count = 0;
 
 function send(res) {
-    res.write(`data: ${count}\n\n`);
+    // Send data to the client without ending the response as we want to send stream of data
+    // if you want to end the response, you can use res.end() after sending the last data
+    res.write(`data: ${count}\n\n`);  
     count++;
+    // if (count > 30) {
+    //     res.end(); // End the stream after sending 30 messages
+    //     return;
+    // }
     setTimeout(() => send(res), 1000);
 }
 
